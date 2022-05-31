98cae9ebe382058cdc9437ed0a4a71dbe9d78579
gbote <gerardorafaelbote@gmail.com>
2022-05-31 17:00:27 -0500

    Updated, 5/31

--- algo-character-count/js/charCount.js
+++ algo-character-count/js/charCount.js
@@ -1,12 +1,12 @@
 exports.charCount = function(string) {
-    let arr = string.split("");
-    let result = {};
-    for (i = 0; i < arr.length; i++) {
-      if (arr[i] in result) {
-        result[arr[i]] += 1;
-      } else {
-        result[arr[i]] = 1;
-      }
+  let arr = string.split("");
+  let result = {};
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] in result) {
+      result[arr[i]] += 1;
+    } else {
+      result[arr[i]] = 1;
     }
-   return result;
+  }
+  return result;
 };
