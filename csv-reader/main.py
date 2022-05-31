2187d0cbb9e72d75dcffd80edd7c31ad7f95875d
gbote <gerardorafaelbote@gmail.com>
2022-05-31 17:10:21 -0500

    Updated, 5/31

--- csv-reader/main.py
+++ csv-reader/main.py
@@ -2,12 +2,9 @@ import csv
 animal_type = input("cats or dogs?\n")
 
 try:
-    data = open(f'./data/{animal_type}.csv')
-    reader = csv.DictReader(data)
-
-    for row in reader:
-        print(f"{row['name']} is a {row['age']} year old {row['breed']}.")
+    with open(f'./data/{animal_type}.csv') as data:
+        reader = csv.DictReader(data)
+        for row in reader:
+            print(f"{row['name']} is a {row['age']} year old {row['breed']}.")
 except Exception:
     print(f"Sorry, we don't have any {animal_type} here.")
-
-
