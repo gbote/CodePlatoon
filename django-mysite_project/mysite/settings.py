5caa96a0d5edef31201b44f80ca010db6c2b883d
gbote <gerardorafaelbote@gmail.com>
2022-06-22 14:39:43 -0500

    Updated django-mysite_project, 6/22

--- django-mysite_project/mysite/settings.py
+++ django-mysite_project/mysite/settings.py
@@ -116,9 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# clients can send requests to this URL to ask for our static files
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# this is a list of directories in our project that we can serve static files froms
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
