1826f25148e840247c299c625fa6cf4c1ed8c554
gbote <gerardorafaelbote@gmail.com>
2022-07-21 23:59:13 -0500

    Completed react-news-site-v, 7/22 12AM

---
+++ react-news-site-v/news-site-v/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import AppNav from './components/AppNav/AppNav.jsx';
+import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
+import ArticlePage from './pages/ArticlePage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import SectionPage from './pages/SectionPage.jsx'
+import _, {debounce} from 'lodash'
+
+
+function App() {
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+      <AppNav />
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/articles/:articleID" element={<ArticlePage />}/>
+          <Route path="/sections/:sectionName" element={<SectionPage />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
